93728c06482d020cc84f41739c0cf8de08c45bc6
minkyeongJ <jomk2221@naver.com>
2022-09-29 13:02:31 +0900

    Bookmark: Asset, Metadata. and CSS

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hi, Min-kyeong here! I`m front-end developer from Korea. You can
+          contact me on <a href="https://velog.io/@fenjo">Blog</a>
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
